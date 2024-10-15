f70c4c570546863df5b414253bd23e48aaa48e50
Guido Castillo <guido.castillo.002@gmail.com>
2024-10-15 17:21:59 +0200

    Videos 4, 5 y 6 del Modulo 7

--- proyectoFinal/index.js
+++ proyectoFinal/index.js
@@ -1,29 +1,26 @@
-const menu = [
-    {
-        pizza: "Margherita",
-        precio: 4
-    },
-    {
-        pizza: "Bufalina",
-        precio: 6
-    },
-    {
-        pizza: "Diavola",
-        precio: 5
-    },
-    {
-        bebida: "Coca-Cola",
-        precio: 2
-    },
-    {
-        bebida: "Fiano",
-        precio: 12
-    }
-]
+const usuario = {
+    nombre: "Guido",
+    edad: 29,
+    deuda: 0
+}
 
+const pedido = []
 
 const showMenu = () => {
-    menu.forEach() {
-        
+    console.log("Codigo - Producto - Precio")
+    for(let producto of menu) {
+        console.log(`${producto.codigo} - ${producto.nombre} - ${producto.precio}`)
     }
+}
+
+
+const pedirProducto = cod => {
+    if (!cod) return "Ingrese un codigo de producto" // Si lo escribo asi en la misma linea no es necesario poner las llaves
+
+    const busqueda = menu.find(producto => producto.codigo === cod)
+    if (!busqueda) return "El producto no existe"
+
+    pedido.push(busqueda) 
+    console.log("El producto se ha agregado al pedido. Su pedido es:")
+    return pedido
 }
---
+++ proyectoFinal/menu.js
@@ -0,0 +1,27 @@
+const menu = [
+    {
+        nombre: "Margherita",
+        precio: 4,
+        codigo: "M100"
+    },
+    {
+        nombre: "Bufalina",
+        precio: 6,
+        codigo: "B100"
+    },
+    {
+        nombre: "Diavola",
+        precio: 5,
+        codigo: "D100"
+    },
+    {
+        nombre: "Coca-Cola",
+        precio: 2,
+        codigo: "C100"
+    },
+    {
+        nombre: "Fiano",
+        precio: 12,
+        codigo: "F100"
+    }
+]
