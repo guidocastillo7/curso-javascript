188a0f066edb860ce42bf7435ef95084f5ea5bed
Guido Castillo <guido.castillo.002@gmail.com>
2024-10-15 15:29:32 +0200

    Videos 1, 2 y 3 del Modulo 7

---
+++ proyectoFinal/index.js
@@ -0,0 +1,29 @@
+const menu = [
+    {
+        pizza: "Margherita",
+        precio: 4
+    },
+    {
+        pizza: "Bufalina",
+        precio: 6
+    },
+    {
+        pizza: "Diavola",
+        precio: 5
+    },
+    {
+        bebida: "Coca-Cola",
+        precio: 2
+    },
+    {
+        bebida: "Fiano",
+        precio: 12
+    }
+]
+
+
+const showMenu = () => {
+    menu.forEach() {
+        
+    }
+}
